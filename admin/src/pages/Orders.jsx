b13b4b29ed14883cd55b9f182eb858f281ee828d
ankitjangid02 <ankitj2811@gmail.com>
2026-05-21 13:29:48 +0530

    updated orders.jsx

--- admin/src/pages/Orders.jsx
+++ admin/src/pages/Orders.jsx
@@ -20,6 +20,8 @@ const Orders = ({ token }) => {
       if (response.data.success) {
         setOrders(response.data.orders.reverse())
       } else {
+        <p>abcd</p>
+
         toast.error(response.data.message)
       }
 
@@ -30,7 +32,7 @@ const Orders = ({ token }) => {
 
   const statusHandler = async (event, orderId) => {
     try {
-      const response = await axios.post(backendUrl + '/api/order/status', {orderId, status:event.target.value}, {headers:{token}})
+      const response = await axios.post(backendUrl + '/api/order/status', { orderId, status: event.target.value }, { headers: { token } })
       if (response.data.success) {
         await fetchAllOrders()
       }
@@ -80,7 +82,7 @@ const Orders = ({ token }) => {
                 <p>Date: {new Date(order.date).toLocaleDateString()}</p>
               </div>
               <p className='text-sm sm:text-[15px]'>{currency} {order.amount}</p>
-              <select onChange={(event)=>statusHandler(event, order._id)} value={order.status} className='p-2 font-semibold'>
+              <select onChange={(event) => statusHandler(event, order._id)} value={order.status} className='p-2 font-semibold'>
                 <option value="Order Placed">Order Placed</option>
                 <option value="Packing">Packing</option>
                 <option value="Shipped">Shipped</option>
