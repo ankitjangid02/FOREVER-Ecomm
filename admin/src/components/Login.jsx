6daead47e95fa4f610c62634144f8d713d491146
ankitjangid02 <ankitj2811@gmail.com>
2026-05-21 12:58:29 +0530

    update deployment config

--- admin/src/components/Login.jsx
+++ admin/src/components/Login.jsx
@@ -11,7 +11,9 @@ const Login = ({setToken}) => {
     const onSubmitHandler = async (e) => {
         try {
             e.preventDefault();
-            const response = await axios.post(backendUrl+'/api/user/admin',{email,password})
+            console.log(backendUrl)
+
+            const response = await axios.post(backendUrl + '/api/user/admin',{ email, password })
             if (response.data.success) {
               setToken(response.data.token)
             }else{
