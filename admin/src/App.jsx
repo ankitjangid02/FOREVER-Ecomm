be1af2c2de6fdbb7a9d7c7232282658346dce1e0
ankitjangid02 <ankitj2811@gmail.com>
2026-05-22 21:05:06 +0530

    added loader to admin

--- admin/src/App.jsx
+++ admin/src/App.jsx
@@ -10,22 +10,44 @@ import Login from './components/Login'
 import { ToastContainer } from 'react-toastify';
 import { useEffect } from 'react'
 
+import { useLocation } from 'react-router-dom'
+import Loader from './components/Loader'
+
 
 export const backendUrl = import.meta.env.VITE_BACKEND_URL
 export const currency = '₹'
 
 const App = () => {
 
-  const [token, setToken] = useState(localStorage.getItem('token') ? localStorage.getItem('token'):'');
 
-  useEffect(()=>{
-    localStorage.setItem('token',token)
-  },[token])
+
+  const location = useLocation()
+
+  const [loading, setLoading] = useState(false)
+
+  useEffect(() => {
+    setLoading(true)
+    const timer = setTimeout(() => {
+      setLoading(false)
+    }, 400)
+
+    return () => clearTimeout(timer)
+
+  }, [location])
+
+
+
+  const [token, setToken] = useState(localStorage.getItem('token') ? localStorage.getItem('token') : '');
+
+  useEffect(() => {
+    localStorage.setItem('token', token)
+  }, [token])
 
   console.log(token);
 
   return (
     <div className='bg-gray-50 min-h-screen'>
+      {loading && <Loader />}
       <ToastContainer />
       {token === ""
         ? <Login setToken={setToken} />
@@ -36,7 +58,7 @@ const App = () => {
             <Sidebar />
             <div className='w-[70%] mx-auto ml-[max(5vw,25px)] my-8 text-gray-600 text-base'>
               <Routes>
-                <Route path='/' element={<Home/>} />
+                <Route path='/' element={<Home />} />
                 <Route path='/add' element={<Add token={token} />} />
                 <Route path='/list' element={<List token={token} />} />
                 <Route path='/orders' element={<Orders token={token} />} />
